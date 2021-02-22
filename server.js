9a234bf1a29279fc997366a735709418efbb5bdf
KiraStaehr <74182118+KiraStaehr@users.noreply.github.com>
2021-02-22 08:07:24 +0100

    audio should be working. the video needs to be turned off.

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+'use strict';
 /*express and node stuff */
 const express = require('express');
 const app = express();
 const path = require('path');
 const http = require('http').createServer(app);
-const PORT = 3000;
+const PORT = 5000;
 
+
+
+//let app2 = require('http').createServer(function(req, res){
+//  fileServer.serve(req, res);
+//}).listen(PORT);
 /*for doing io stuff*/
 const io = require('socket.io')(http);
+let io2=io.listen(http);
 
 /*our modules*/
 const user = require("./scripts/user");
@@ -52,6 +59,58 @@ io.on('connection', (socket) => {
     user.deleteID(socket.id);
     user.showAll();
   });
+//***********TEST**************///
+  // convenience function to log server messages on the client
+  function log() {
+    let array = ['Message from server:'];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+
+  socket.on('message', function(message) {
+    log('Client said: ', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('create or join', function(room) {
+    log('Received request to create or join room ' + room);
+
+    let clientsInRoom = io.sockets.adapter.rooms[room];
+    let numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (numClients === 0) {
+      socket.join(room);
+      log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+
+    } else if (numClients === 1) {
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      io2.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io2.sockets.in(room).emit('ready');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('ipaddr', function() {
+    let ifaces = os.networkInterfaces();
+    for (let dev in ifaces) {
+      ifaces[dev].forEach(function(details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
+  });
+
+  socket.on('bye', function(){
+    console.log('received bye');
+  });
+//***********TEST**************///
 });
 
 
